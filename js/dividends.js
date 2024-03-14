475bbff03c3d9f3341a74fffe6503749d4fafa0a
Susan Mwiberi <susan.mwiberi@gmail.com>
2024-03-14 14:12:26 +0300

    added calculator

--- js/dividends.js
+++ js/dividends.js
@@ -1,7 +1,8 @@
 function calculateDepositDividends(
   startingDeposit,
   monthlyDeposit,
-  interestRate
+  interestRate,
+  withholdingTaxRate
 ) {
   let error = "";
   let response = "";
@@ -14,9 +15,27 @@ function calculateDepositDividends(
       error = "Monthly contribution is required";
     } else if (!interestRate) {
       error = "Deposits Interest is required";
+    } else if (!withholdingTaxRate) {
+      error = "Withholding tax rate is required";
     } else {
       //   calculate dividends
-      response = "Request is valid " + happyEmoji;
+      startingDepositDividends = (interestRate * startingDeposit) / 100;
+      yearDividends = getDividendsPerYear(monthlyDeposit, interestRate);
+      totalDividends = startingDepositDividends + yearDividends;
+      totalTax = (totalDividends * withholdingTaxRate) / 100;
+      dividendsAfterTax = totalDividends - totalTax;
+
+      response =
+        "Dividends on deposits: " +
+        startingDepositDividends +
+        "</br>Dividends earned withing the year: " +
+        yearDividends +
+        "</br>Total Dividends: " +
+        totalDividends +
+        "</br>Total Tax: " +
+        totalTax +
+        "</br>Total earnings after tax: " +
+        dividendsAfterTax;
     }
   } catch (err) {
     error = err;
@@ -28,9 +47,20 @@ function calculateDepositDividends(
 
   if (!error) {
     document.getElementById("response").innerHTML = response + happyEmoji;
+
     document.getElementById("error").innerHTML = "";
   } else {
     document.getElementById("error").innerHTML = error + sadEmoji;
     document.getElementById("response").innerHTML = "";
   }
 }
+
+function getDividendsPerYear(monthlyDeposit, interestRate) {
+  interest = 0;
+  for (i = 1; i <= 11; i++) {
+    interest = interest + (((monthlyDeposit * i) / 12) * interestRate) / 100;
+  }
+  return interest;
+}
+
+console.log(getDividendsPerYear(50000, 10));
